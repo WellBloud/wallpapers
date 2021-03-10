2e4260fb602c6061bf0392eb5f836e981f1c1ff7
Peter Holop <pholop@lumenad.com>
2021-03-10 11:23:48 +0100

    added script for generating + new wallpapers

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+truncate -s 0 backgrounds.json
+echo "[" >> backgrounds.json
+
+for i in wallpapers/*
+    do
+        echo "{\"url\": \"https://raw.githubusercontent.com/WellBloud/wallpapers/master/wallpapers/$i\"}," >> backgrounds.json
+done
+
+echo "{}]" >> backgrounds.json
