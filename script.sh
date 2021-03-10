39068abafc8d2a6a141dd349047636faed03594e
Peter Holop <pholop@lumenad.com>
2021-03-10 11:27:18 +0100

    fixed path

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ echo "[" >> backgrounds.json
 
 for i in wallpapers/*
     do
-        echo "{\"url\": \"https://raw.githubusercontent.com/WellBloud/wallpapers/master/wallpapers/$i\"}," >> backgrounds.json
+        echo "{\"url\": \"https://raw.githubusercontent.com/WellBloud/wallpapers/master/$i\"}," >> backgrounds.json
 done
 
 echo "{}]" >> backgrounds.json
