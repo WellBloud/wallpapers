eb4767fa98efc0b33c0acf08a3672cd9fe7ab5d6
Peter Holop <pholop@lumenad.com>
2022-01-06 12:00:06 +0100

    added winter

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 truncate -s 0 backgrounds.json
 echo "[" >> backgrounds.json
 
-for i in wallpapers/*
+for i in winter/*
     do
         echo "{\"url\": \"https://raw.githubusercontent.com/WellBloud/wallpapers/master/$i\"}," >> backgrounds.json
 done
