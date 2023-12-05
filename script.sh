c217923a67c641b4bdecfd3bd19316ddd29e91fd
Peter Holop <pholop@lumenad.com>
2023-12-05 13:59:55 +0100

    switching to winter

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 truncate -s 0 backgrounds.json
 echo "[" >> backgrounds.json
 
-for i in autumn/*
+for i in winter/*
     do
         echo "{\"url\": \"https://raw.githubusercontent.com/WellBloud/wallpapers/master/$i\"}," >> backgrounds.json
 done
